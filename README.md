b6c55bc099574968c17a12524b9770733568e78c
fatemeh nassari zadeh <nassarifa98@gmail.com>
2025-04-11 15:27:31 +0330

    add migrate scripts

---
+++ README.md
@@ -0,0 +1,12 @@
+# 🚀 Blog Development Project
+
+## 🌐 English Version
+
+### 👋 Hello Everyone!
+This project is a developed blogging platform.
+
+### 🛠️ How to Run the Project
+First, you need to run the following scripts:
+
+```bash
+npm run migrate
---
+++ migrations/index.js
@@ -0,0 +1,23 @@
+// ? RUN QUERIES:
+const fs = require("fs");
+const path = require("path");
+const db = require("../db")
+
+const migrate = async () => {
+    const create_Users_Table = fs.readFileSync(path.resolve(__dirname, './users-ddl.sql'), "utf-8");
+    const create_Articles_Table = fs.readFileSync(path.resolve(__dirname, './articles-ddl.sql'), "utf-8");
+    const create_Tags_Table = fs.readFileSync(path.resolve(__dirname, './tags-ddl.sql'), "utf-8");
+    const create_Article_Tag_Table = fs.readFileSync(path.resolve(__dirname, './articles_tags-ddl.sql'), "utf-8");
+
+    try {
+        db.query(create_Users_Table)
+        db.query(create_Articles_Table)
+        db.query(create_Tags_Table)
+        db.query(create_Article_Tag_Table)
+
+    } 
+    catch (error) {
+        throw `Error happened in migrations: \n ${error}`
+    }
+}
+migrate()
