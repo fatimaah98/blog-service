75259ea7fc471c87f1565b24b1c0a016d766836c
fatemeh nassari zadeh <nassarifa98@gmail.com>
2025-04-11 14:28:29 +0330

    set inital configs

---
+++ config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    db: {
+        user: process.env.DB_USER,
+        port: process.env.DB_PORT,
+        host: process.env.DB_HOST,
+        password: process.env.DB_PASSWORD,
+        name: process.env.DB_NAME
+    },
+
+    port: parseInt(process.env.PORT) || 4000,
+    
+    auth: {
+        accessTokenExpireTime: process.env.ACCESS_TOKEN_EXPERTS_IN_SECONDS,
+        refereshTokenExpireTime: process.env.REFERESH_TOKEN_EXPERTS_IN_SECONDS,
+        accessTokenKey: process.env.ACCESS_TOKEN_SECRETkEY,
+        refereshTokenKey: process.env.REFERESH_TOKEN_SECRETkEY,
+    }
+}
---
+++ db.js
@@ -0,0 +1,20 @@
+// ? connect data base:
+const mysql = require('mysql2');
+const configs = require('./config')
+
+const connection = mysql.createConnection({
+    host: configs.db.host,
+    port: configs.db.port,
+    user: configs.db.user,
+    password: configs.db.password,
+    database: configs.db.name
+})
+
+connection.connect((err) => {
+    if(err) {
+        throw "error in connection: " + err
+    }
+    console.log("connected...");
+})
+
+module.exports = connection;
